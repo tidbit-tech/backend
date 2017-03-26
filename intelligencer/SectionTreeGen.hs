640c3f4642262d4550bd5d06ecfce6320a29e0d2
Alastair Paragas <alastairparagas@gmail.com>
2017-03-26 07:34:00 -0400

    SectionTreeGen Module

---
+++ intelligencer/SectionTreeGen.hs
@@ -0,0 +1,88 @@
+module SectionTreeGen where
+
+import Prelude hiding (concat)
+import Data.List (foldl')
+import Data.Graph.Inductive.PatriciaTree
+  (Gr)
+import Data.Graph.Inductive.Graph 
+  (LNode
+  , LEdge
+  , mkGraph)
+import Text.HTML.TagSoup 
+  (Tag (TagText))
+import Text.HTML.TagSoup.Tree 
+  (TagTree (TagBranch, TagLeaf)
+  , parseTree
+  )
+import Glider.NLP.Language.English.Porter (stem)
+import Glider.NLP.Tokenizer
+  (getWords
+  , tokenize)
+import Data.Text 
+  (pack
+  , unpack
+  , concat)
+
+
+type SectionTree = Gr SectionTreeNode String
+data SectionTreeNode = 
+  Root | ContainerNode | Node {
+    rawContent :: String, 
+    normalizedContent :: String
+  } deriving (Eq, Show)
+
+
+htmlToSectionTrees :: String -> SectionTree
+htmlToSectionTrees htmlBody = 
+  uncurry mkGraph $ buildNormalizedTree 0 1 ([(0, Root)], []) parsed
+  where 
+    
+    parsed :: TagTree String
+    parsed = head (parseTree htmlBody)
+    
+    normalizeContent :: String -> String
+    normalizeContent stringContent = 
+      (unpack . concat . getWords . tokenize . stem . pack) stringContent
+      
+    createNewChildNode :: Int 
+                          -> Int
+                          -> ([LNode SectionTreeNode], [LEdge String])
+                          -> String
+                          -> String
+                          -> ([LNode SectionTreeNode], [LEdge String])
+    createNewChildNode parNode curNode (nodes, edges) "" rel = 
+      (
+        (curNode, ContainerNode):nodes, 
+        (parNode, curNode, rel):edges
+      )
+    createNewChildNode parNode curNode (nodes, edges) content rel = 
+      (
+        (curNode, Node {
+          rawContent = content,
+          normalizedContent = normalizeContent content
+        }):nodes, 
+        (parNode, curNode, rel):edges
+      )
+      
+    buildNormalizedTree :: Int 
+                          -> Int 
+                          -> ([LNode SectionTreeNode], [LEdge String])
+                          -> TagTree String
+                          -> ([LNode SectionTreeNode], [LEdge String])
+    buildNormalizedTree parNode curNode x (TagLeaf (TagText content)) = 
+      createNewChildNode parNode curNode x content "text"
+    buildNormalizedTree _ _ x (TagLeaf _) = 
+      x
+    buildNormalizedTree parNode curNode x (TagBranch rel _ tagTrees) = 
+      foldl' 
+        reducer
+        initSectionTreeTuple 
+        (zip tagTrees [firstChildNode..lastChildNode])
+      where
+        initSectionTreeTuple@((newParNode,_):_, _) = 
+          createNewChildNode parNode curNode x "" rel
+        firstChildNode = newParNode + 1
+        lastChildNode = firstChildNode + (length tagTrees) - 1
+        reducer sectionTreeTuple (tagTree, curNode) = 
+          buildNormalizedTree newParNode curNode sectionTreeTuple tagTree
+      
